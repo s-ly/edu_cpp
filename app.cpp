06c92589a0b6b123c5a80cf51a7d34a982274c4d
s-ly <sergeylysov@outlook.com>
2024-08-06 12:38:51 +0300

    constructor copy

--- app.cpp
+++ app.cpp
@@ -2,13 +2,15 @@
 
 class MyClass {
  public:
-  MyClass() { std::cout << "Constructor 1" << std::endl; }
-  MyClass(int x) : myVar1(x) { std::cout << "Constructor 2" << std::endl; }
-  MyClass(int x, int y) : myVar1(x), myVar2(y) {
-    std::cout << "Constructor 3" << std::endl;
-  }
+  MyClass() {}
+  MyClass(int x) : myVar1(x) {}
+  MyClass(int x, int y) : myVar1(x), myVar2(y) {}
 
-  ~MyClass() { std::cout << "Destructor" << std::endl; }
+  // конструктор копирования
+  MyClass(const MyClass& other) : myVar1(other.myVar1), myVar2(other.myVar2) {}
+
+  ~MyClass() {}
+
   void setVar(int value) { myVar1 = value; }
   void setVar(int value1, int value2) {
     myVar1 = value1;
@@ -38,16 +40,15 @@ class MyClass {
 };
 
 int main() {
-  std::cout << "Hi!" << std::endl;
   MyClass obj1(11, 12);
-  MyClass obj2(13, 14);
+
+  MyClass obj2(obj1);
+
   std::cout << "Value: " << obj1.getVar(1) << ", " << obj1.getVar(2)
             << std::endl;
+
   std::cout << "Value: " << obj2.getVar(1) << ", " << obj2.getVar(2)
             << std::endl;
 
-  MyClass obj_result = obj1 + obj2;
-  std::cout << "Value: " << obj_result.getVar(1) << ", " << obj_result.getVar(2)
-            << std::endl;
   return 0;
 }
